380428b435c57b8312f4600348e57e9a99d1c3c3
Souvik Roy <souvikroy@Souviks-MacBook-Air.local>
2024-11-26 01:25:16 +0530

    installed along with Tailwind-CSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-500'>
+         <h1>Souvik</h1>
+         <h1>Kishaya</h1>
+    </div>
+  )
+}
+
+export default App
